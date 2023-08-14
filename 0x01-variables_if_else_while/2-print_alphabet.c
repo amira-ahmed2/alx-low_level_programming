cf86df99d3e9fe099b86c02cc2fbf02cfc5f6f31
amira-ahmed2 <amiraa.amin300@yahoo.com>
2023-08-14 06:32:26 -0700

    add file

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,19 +2,19 @@
 #include <stdlib.h>
 #include <time.h>
 
- /**
- * main - prints the alphabet in lowercase,followed by a new line
- * a alphabet to the variable n
- * Return: Always 0 (Success)
- */
+/**
+* main - prints the alphabet in lowercase,followed by a new line
+* description : alphabet to the variable c
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-    char c;
-    c = 'a';
-    while(c <= 'z'){
-              putchar(c);
-              c = c+1;
-      }
-    exit(EXIT_SUCCESS);
-	return (0);
+char c;
+c = 'a';
+while(c <= 'z'){
+putchar(c);
+c = c+1;
+}
+exit(EXIT_SUCCESS);
+return (0);
 }

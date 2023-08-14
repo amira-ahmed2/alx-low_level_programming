9ed22396f41bd1bd3c129131f1ae5dac449db376
amira-ahmed2 <amiraa.amin300@yahoo.com>
2023-08-14 07:48:34 -0700

    add file

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - prints the alphabet in lowercase, and then in uppercase, followed by a new line
+* description : alphabet to the variable cUpper,cLow
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char cLow;
+char cUpper;
+cLow = 'a';
+cUpper = 'A';
+while (cLow <= 'z')
+{
+putchar(cLow);
+cLow = cLow + 1;
+}
+putchar('\n');
+while (cUpper <= 'Z')
+{
+putchar(cUpper);
+cUpper = cUpper + 1;
+}
+putchar('\n');
+return (0);
+}

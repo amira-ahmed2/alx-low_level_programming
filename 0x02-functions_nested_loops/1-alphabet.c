489d0d0569fbddfd879e0203838be8c92466aa5c
amira-ahmed2 <amiraa.amin300@yahoo.com>
2023-08-15 10:10:30 -0700

    add file

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+* main - prints the alphabet in lowercase,followed by a new line
+* description : alphabet to the variable c
+* Return: Always 0 (Success)
+*/
+
 void print_alphabet(void)
 {
 char c;

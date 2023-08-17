bec3cfe573ca8f1081ad645dbb0a08f65b37bd65
amira-ahmed2 <amiraa.amin300@yahoo.com>
2023-08-17 16:40:13 -0700

    add file

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,3 +7,24 @@
  *
  * @size : is pramiter to print
  */
+void print_triangle(int size)
+{
+int i ,j;
+
+if (size <= 0)
+_putchar('\n');
+else
+{
+for (i = 1; i<= size; i++)
+{
+for (j = 1; j<= size; j++)
+{
+if ((i + j) <= size)
+_putchar(' ');
+else
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+}

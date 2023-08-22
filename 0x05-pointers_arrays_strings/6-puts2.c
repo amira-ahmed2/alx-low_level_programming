3f2f83011985b3e4ff796b2a9753057742b0d84d
amira-ahmed2 <amiraa.amin300@yahoo.com>
2023-08-21 17:03:35 -0700

    add file

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * @str: character to compare
+ *
+ */
+void puts2(char *str)
+{
+int i;
+char temp;
+
+for (int i = 0; str[i] != 0; ++i)
+{
+if (i % 2 == 0)
+putchar(str[i]);
+}
+putchar('\n');
+}

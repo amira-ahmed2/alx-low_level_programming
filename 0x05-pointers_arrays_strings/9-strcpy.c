af1738fd8cc1d211775ee1dcae009cc9ca033c59
amira-ahmed2 <amiraa.amin300@yahoo.com>
2023-08-21 19:09:33 -0700

    add file

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include<main.h>
+
+/**
+ * *_strcpy - that copies the string pointed to by src
+ * @dest: char one to compare
+ * @src: char two to compare
+ * Return: a character value is dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+for (int i = -1; i <= src[i] ; i++)
+{
+dest[i] = src[i];
+}
+return (dest);
+}

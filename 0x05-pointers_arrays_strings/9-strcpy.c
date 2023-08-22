b34ac5a7feb4a164809de9a80ef907aed89793d2
amira-ahmed2 <amiraa.amin300@yahoo.com>
2023-08-21 19:18:15 -0700

    add file

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,10 +8,11 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i;
-for (i = -1; i <= src[i] ; i++)
-{
+int i = -1;
+
+do {
+i++;
 dest[i] = src[i];
-}
+} while (src[i] != '\0');
 return (dest);
 }

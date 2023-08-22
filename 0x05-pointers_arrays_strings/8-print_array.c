ea6de15175374406ae7706a9ce631ebd15f70f31
amira-ahmed2 <amiraa.amin300@yahoo.com>
2023-08-21 18:32:55 -0700

    add file

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_array - prints n elements of an array of integers
+ * @a: number to compare
+ * @n: number to compare
+ */
+void print_array(int *a, int n)
+{
+int i;
+
+for (i = 0; i < n; i++)
+{
+printf("%d", a[i]);
+if (i != n - 1)
+printf(", ");
+}
+putchar("\n");
+}

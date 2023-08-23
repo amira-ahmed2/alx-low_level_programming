1b1e8dcb451e4314e1c6c85c4e6476cc9f78b0ca
amira-ahmed2 <amiraa.amin300@yahoo.com>
2023-08-23 15:36:50 -0700

    add file

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -7,21 +7,17 @@
  * @n: number of entry times
  * Return: char value dest
  */
-ichar *_strncpy(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
 int j;
 
-j = 0;
-while (j < n  && src[j] != '\0')
+for (j = 0;  j < n  && src[j] != '\0'; j++)
 {
 dest[j] = src[j];
-j++;
 }
-while (j < n)
+for (; j < n; j++)
 {
 dest[j] = 0;
-j++;
 }
-
 return (dest);
 }

3d7fc6ae93e48e1cd124bd189341ce27d8e3e9e1
amira-ahmed2 <amiraa.amin300@yahoo.com>
2023-08-23 15:50:20 -0700

    add file

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,11 +9,12 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-int j;
+int j = 0;
 
-for (j = 0;  j < n  && src[j] != '\0'; j++)
+while (j < n  && src[j] != '\0')
 {
 dest[j] = src[j];
+j++;
 }
 while (j < n)
 {

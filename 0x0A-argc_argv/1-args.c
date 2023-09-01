188eb2c7aa6307b3bcead846996e0fb29eed755a
amira-ahmed2 <amiraa.amin300@yahoo.com>
2023-09-01 16:17:24 -0700

    add f

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints its name
+ * @argc: lenght argv
+ * @argv: array
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+(void)argv;
+printf("%d\n", argc - 1);
+return (0);
+}

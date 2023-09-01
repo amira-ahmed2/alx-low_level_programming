db4eb388b79026681368d729d44e39d3f22594d8
amira-ahmed2 <amiraa.amin300@yahoo.com>
2023-09-01 16:33:54 -0700

    add f

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints all arguments it receives.
+ * @argc: lenght argv
+ * @argv: array
+ * Return: 0
+ */
+int main(int argc, char const *argv[])
+{
+int itr = 0;
+
+while (argc--)
+{
+printf("%s\n", argv[i]);
+i++;
+}
+return (0);
+}

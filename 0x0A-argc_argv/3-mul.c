9afcced38642cb28830f77ee34b10e07ecc528dc
amira-ahmed2 <amiraa.amin300@yahoo.com>
2023-09-01 17:15:26 -0700

    add f

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - that multiplies two numbers.
+ * @argc: lenght argv
+ * @argv: array
+ * Return: 1 in error
+ */
+int main(int argc, char const *argv[])
+{
+int resultSum;
+
+if (argc == 3)
+{
+resultSum = atoi(argv[1]) * atoi(argv[2]);
+printf("%d\n", resultSum);
+}
+else
+{
+printf("Error\n");
+return (1);
+}
+}
---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - that adds positive numbers.
+ * @argc: lenght argv
+ * @argv: array
+ * Return: resultsum in error
+ */
+int main(int argc, char const *argv[])
+{
+int resultSum;
+char *ch;
+
+while (--argc)
+{
+for (ch = argv[argc]; *ch; ch++)
+{
+if (*ch < '0' || *ch > '9')
+return (printf("Error\n", 1));
+resultSum += atoi(argv[argc]);
+}
+printf("%d\n", resultSum);
+}
+}

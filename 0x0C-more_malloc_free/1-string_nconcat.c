dd72d3d769e4aa5a0527dde8b2fcb71a484500ab
amira-ahmed2 <amiraa.amin300@yahoo.com>
2023-09-07 11:31:34 -0700

    add file

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,13 +11,19 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 char *ch;
+unsigned int i, j, len_s1, len_s2;
 
 if (s1 == NULL)
 return ("");
 if (s2 == NULL)
 return ("");
 
-ch = malloc(strlen(s1) + n + 1);
+for (len_s1 = 0; s1[len_s1] != '\0'; len_s1++)
+;
+for (len_s2 = 0; s2[len_s2] != '\0'; len_s2++)
+;
+
+ch = malloc(len_s1 + n + 1);
 if (ch == NULL)
 {
 return (NULL);

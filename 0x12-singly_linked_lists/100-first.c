71ddc9f2490bf237b114430b1f9167403ff05eb5
AmiraAhmed <106966309+amira-ahmed2@users.noreply.github.com>
2023-09-21 23:35:56 +0300

    Create 100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,7 @@
+/**
+ * before_main -  function that prints executed before main
+ * Return: no return.
+ */
+void __attribute__((constructor)) before_main(){
+   printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}

0e1be784685c53260ecd043fb7a70a38db4019cb
AmiraAhmed <106966309+amira-ahmed2@users.noreply.github.com>
2023-09-21 23:20:38 +0300

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,6 +2,22 @@
 #include <string.h>
 
 /**
+ * _strlen - Write a function that returns the length of a string
+ *
+ * @s: value it points
+ * Return: returns length
+ */
+int _strlen(char *s)
+{
+int cont = 0;
+
+for (; *s != '\0'; s++)
+cont++;
+
+return (cont);
+}
+
+/**
  * print_list - prints all the elements of a list_t list
  * @h: the first node
  *
@@ -10,12 +26,13 @@
 size_t print_list(const list_t *h)
 {
 size_t i;
-for (i = 0; h != 0; i++)
+
+for (i = 0; h; i++)
 {
 if (h->str)
-printf("[%d] %s\n", strlen(h->str), h->str);
+printf("[%d] %s\n", _strlen(h->str), h->str);
 else
-printf("[%d] %s\n", strlen(h->str), "(nil)");
+printf("[%d] %s\n", _strlen(h->str), "(nil)");
 h = h->next;
 }
 return (i);

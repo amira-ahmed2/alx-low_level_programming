f132271de912067da6acbf9ea2b344b7a971d084
AmiraAhmed <106966309+amira-ahmed2@users.noreply.github.com>
2023-10-03 01:27:59 +0300

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,7 +10,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-#define R_BUFF_S 1024;
+#define READ_BUF_SIZE 1024;
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);

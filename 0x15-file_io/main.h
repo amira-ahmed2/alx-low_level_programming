0c0a602195f600630916e218e77c1ff03d06744c
amira-ahmed2 <amiraa.amin300@yahoo.com>
2023-10-02 14:32:32 -0700

    add

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,7 +5,10 @@
 #include <stdlib.h>
 #include <string.h>
 
-
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 #define R_BUFF_S 1024;
 
 ssize_t read_textfile(const char *filename, size_t letters);

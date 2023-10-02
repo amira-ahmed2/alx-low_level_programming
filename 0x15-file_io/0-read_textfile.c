0a9f59bcaf85e500f00b5ac5ef90a217eec64f7d
AmiraAhmed <106966309+amira-ahmed2@users.noreply.github.com>
2023-10-03 01:29:40 +0300

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,7 +10,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 int f;
 ssize_t bs;
-	char buff[READ_BUF_SIZE * 4];
+	char buff[R_BUFF_S * 8];
 
 	if (!filename || !letters)
 	return (0);

b90d33b77467a8746b68295afe924a5193417edc
AmiraAhmed <106966309+amira-ahmed2@users.noreply.github.com>
2023-10-03 00:23:41 +0300

    Create 1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * create_file - that creates a file.
+ * @filename: the file name
+ * @text_content: the strings
+ * Return: the number
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int f;
+	int b = 0, lenth = strlen(text_content);
+
+	if (!filename)
+	{
+		/* code */
+		return (-1);
+	}
+
+	f = open(filename, O_WRONLY | O_CREAT | O_TRUNC | S_IRUSR | S_IWUSR);
+	if (f == -1)
+	{
+		/* code */
+		return (-1);
+	}
+	if (lenth)
+	{
+		/* code */
+		b = write(f, text_content, lenth);
+	}
+	close(f);
+	return (b == lenth ? 1 : -1);
+}

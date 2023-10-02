0ad34159a074040a2f6020f596efeacb3e4c7f0b
AmiraAhmed <106966309+amira-ahmed2@users.noreply.github.com>
2023-10-03 00:38:03 +0300

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,12 +9,12 @@
 int create_file(const char *filename, char *text_content)
 {
 	int f;
-	int b = 0, lenth = strlen(text_content);
+	ssize_t b = 0, lenth = strlen(text_content);
 
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC | S_IRUSR | S_IWUSR);
 	if (f == -1)
 	{
 		/* code */

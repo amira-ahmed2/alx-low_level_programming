37bba4712f7af73113efdd11b4fe0dcfb846ab98
AmiraAhmed <106966309+amira-ahmed2@users.noreply.github.com>
2023-10-03 00:42:21 +0300

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,6 +1,22 @@
 #include "main.h"
 
 /**
+ * _strlen - Write a function that returns the length of a string
+ *
+ * @s: value it points
+ * Return: returns length
+ */
+int _strlen(char *s)
+{
+int cont;
+
+for (cont = 0; *s != '\0'; s++)
+cont++;
+
+return (cont);
+}
+
+/**
  * create_file - that creates a file.
  * @filename: the file name
  * @text_content: the strings
@@ -9,7 +25,7 @@
 int create_file(const char *filename, char *text_content)
 {
         int f;
-        int b = 0, lenth = strlen(text_content);
+        int b = 0, lenth = _strlen(text_content);
 
         if (!filename)
         {

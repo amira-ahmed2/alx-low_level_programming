5199d7c6f79dd39b6d2bd9c12959e6d3c8512939
AmiraAhmed <106966309+amira-ahmed2@users.noreply.github.com>
2023-11-09 20:10:28 +0200

    Update 1-dlistint_len.c

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -2,14 +2,15 @@
 
 /**
  * dlistint_len - that returns the number of elements
- *
  * @h: head first data.
+ * Return: length double list
  */
 size_t dlistint_len(const dlistint_t *h)
 {
 size_t counter = 0;
 
-while (h != NULL) {
+while (h != NULL)
+{
 h = h->next;
 counter++;
 }

65cdf5c8f045e4cce6b0ddf21202a97a9e24b3fc
AmiraAhmed <106966309+amira-ahmed2@users.noreply.github.com>
2023-11-13 20:54:16 +0200

    add file

---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer.
+ * @n: number to compare
+ * Return: always 0;
+ *
+ */
+int _abs(int n)
+{
+if (n < 0)
+n = (-1) * n;
+return (n);
+}
